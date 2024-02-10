d875c5f8ab30b55363648351b87c87d60ec08dfa
yuyun0310 <yl11823@ic.ac.uk>
2024-02-10 10:14:15 +0000

    update compression

--- bonito/cli/compress.py
+++ bonito/cli/compress.py
@@ -130,6 +130,7 @@ def main(args):
     torch.save(quantized_model.state_dict(), os.path.join(workdir, "quantized_model.tar"))
     # torch.save(quantized_model.state_dict(), quantized_model_path)
 
+    quantized_model.to(args.device)
     print('*'*50)
     evaluate_model(args, quantized_model, train_loader)
     print('*'*50)
